928c6ea48eb8dd257856c81136bbdb99cd153108
Charlette Tulod <charlette.tulod@cit.edu>
2023-02-13 21:07:45 +0800

    Publish windows form application

--- Bobskie_HopfieldNetwork/Bobskie_HopfieldNetwork/Form1.Designer.cs
+++ Bobskie_HopfieldNetwork/Bobskie_HopfieldNetwork/Form1.Designer.cs
@@ -103,7 +103,7 @@
             this.button19.Name = "button19";
             this.button19.Size = new System.Drawing.Size(104, 32);
             this.button19.TabIndex = 18;
-            this.button19.Text = "Learn";
+            this.button19.Text = "Solve";
             this.button19.UseVisualStyleBackColor = false;
             this.button19.Click += new System.EventHandler(this.button19_Click);
             // 
